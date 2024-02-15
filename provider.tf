38c0944489387cbcfb003c9eda890bbfd61f2eab
Nils Müller <nils@techtales.io>
2024-02-16 00:51:43 +0100

    feat(vault): remove token variable and use vault data source instead

--- provider.tf
+++ provider.tf
@@ -1,6 +1,10 @@
-provider "discord" {
-  token = var.discord_token
+provider "vault" {
 }
 
-provider "vault" {
+data "vault_generic_secret" "terraform_discord" {
+  path = "infra/techtales/terraform-discord"
+}
+
+provider "discord" {
+  token = data.vault_generic_secret.terraform_discord.data["discord_token"]
 }
