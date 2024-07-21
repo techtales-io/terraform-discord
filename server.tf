9c79ee6a7b63c16e55aff7f8a8aa0cd36e515fac
Nils Müller <tyriis@users.noreply.github.com>
2024-07-21 22:20:15 +0200

    fix: server region europe is invalid on tofu-controller

--- server.tf
+++ server.tf
@@ -3,7 +3,7 @@
 # --------------------------------------------------------------------------------
 resource "discord_server" "techtales" {
   name                          = "techtales.io"
-  region                        = "europe"
+  region                        = "rotterdam"
   afk_channel_id                = "1028684203711271043"
   afk_timeout                   = 900
   default_message_notifications = 1
