512a0f37f2ea828c71091ebecba54c28f1eb89fc
Nils Müller <tyriis@users.noreply.github.com>
2025-04-11 23:49:37 +0200

    fix: terraform version and info channel

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,8 +5,8 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    force_path_style            = true
     skip_requesting_account_id  = true
     skip_s3_checksum            = true
+    use_path_style              = true
   }
 }
